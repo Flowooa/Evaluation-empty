3212a25684ed0b3eb50fa9c5fc6bdbfc541c6149
flowooa <t.trouillez@etudiant.ecolemelies.fr>
2022-05-20 11:49:51 +0200

    Little WIP

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -2,4 +2,25 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
+    public BoxCollider2D playZone;
+    Vector3 tempMaxSpawnPos, tempMinSpawnPos;
+
+    
+
+
+    private void Start()
+    {
+        playZone = GetComponent<BoxCollider2D>();
+        tempMinSpawnPos = playZone.bounds.min;
+        tempMaxSpawnPos = playZone.bounds.max;
+
+      
+
+
+    }
+
+    public void SpawnFruit()
+    {
+        
+    }
 }
--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -1,9 +1,47 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+
 
 public class SnakeController : MonoBehaviour
 {
+    private Vector2 v2;
+    private Vector2Int currentInput;
+
+    public float timeBeforeMove;
+    private Rigidbody2D rb;
+    private bool IsAlive;
+
+    private Vector2 emptySpace;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        if (IsAlive)
+        StartCoroutine(Move());
+        v2 = new Vector2();
+    }
     public void OnMove(InputAction.CallbackContext context)
     {
+        if (!context.performed)
+            return;
+  
+        currentInput = Vector2Int.RoundToInt(v2);
+
+        Debug.Log(currentInput);
+        
+    }
+
+    IEnumerator Move()
+    {
+
+        rb.MovePosition(rb.position + v2 * timeBeforeMove);
+
+        yield return new WaitForSeconds(timeBeforeMove);
+           
     }
 }
